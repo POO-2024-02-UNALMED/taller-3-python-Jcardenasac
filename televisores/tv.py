09b605e938b56c3575dd828555cf299b42e4937b
Jcardenasac <jcardenasac@unal.edu.co>
2024-11-17 20:11:29 -0500

    Update tv.py

--- televisores/tv.py
+++ televisores/tv.py
@@ -1,48 +1,51 @@
+from televisores.control import Control
+
+
 class TV:
     _canal = 1
     _volumen = 1
     _precio = 500
     _numTV = 0
-    _control = None
+    _control = Control()
 
-    def __init__(self,marca,estado):
+    def __init__(self, marca, estado):
         self._marca = marca
         self._estado = estado
 
     def getMarca(self):
         return self._marca
 
-    def setMarca(self,marca):
-        self._nombre = marca
+    def setMarca(self, marca):
+        self._marca = marca
 
     def getCanal(self):
         return self._canal
 
-    def setCanal(self,canal):
+    def setCanal(self, canal):
         self._canal = canal
-    
+
     def getPrecio(self):
         return self._precio
 
-    def setPrecio(self,precio):
+    def setPrecio(self, precio):
         self._precio = precio
 
     def getVolumen(self):
         return self._volumen
 
-    def setVolumen(self,volumen):
+    def setVolumen(self, volumen):
         self._volumen = volumen
 
     def getControl(self):
         return self._control
 
-    def setControl(self,control):
-        self._nombre = control
+    def setControl(self, control):
+        self._control = control
 
     @classmethod
     def getNumTV(self):
         return self._numTV
-    
+
     @classmethod
     def setNumTV(self, numTV):
         self._numTV = numTV
@@ -55,19 +58,19 @@ class TV:
 
     def getEstado(self):
         return self._estado
-    
+
     def canalUp(self):
-        if self._canal != 120 and self._estado == True : 
+        if self._canal != 120 and self._estado == True:
             self._canal += 1
 
     def canalDown(self):
-        if self._canal != 1 and self._estado == True :
+        if self._canal != 1 and self._estado == True:
             self._canal -= 1
 
     def volumenUp(self):
-        if self._volumen != 7 and self._estado == True :
+        if self._volumen != 7 and self._estado == True:
             self._volumen += 1
 
     def volumenDown(self):
-        if self._volumen != 0 and self._estado == True :
+        if self._volumen != 0 and self._estado == True:
             self._volumen -= 1
