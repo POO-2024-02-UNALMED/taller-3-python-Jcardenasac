b3c599be31586f03ba84bd67ab7f3af424de2f9e
Jcardenasac <jcardenasac@unal.edu.co>
2024-11-17 21:14:56 -0500

    Update tv.py

--- televisores/tv.py
+++ televisores/tv.py
@@ -1,7 +1,7 @@
 class TV:
     numTV = 0
 
-    def __init__(self,marca,estado):
+    def __init__(self, marca, estado):
         self._marca = marca
         self._estado = estado
         self._canal = 1
@@ -13,37 +13,37 @@ class TV:
     def getMarca(self):
         return self._marca
 
-    def setMarca(self,marca):
+    def setMarca(self, marca):
         self._marca = marca
 
     def getCanal(self):
         return self._canal
 
-    def setCanal(self,canal):
+    def setCanal(self, canal):
         self._canal = canal
-    
+
     def getPrecio(self):
         return self._precio
 
-    def setPrecio(self,precio):
+    def setPrecio(self, precio):
         self._precio = precio
 
     def getVolumen(self):
         return self._volumen
 
-    def setVolumen(self,volumen):
+    def setVolumen(self, volumen):
         self._volumen = volumen
 
     def getControl(self):
         return self._control
 
-    def setControl(self,control):
+    def setControl(self, control):
         self._control = control
 
     @classmethod
     def getNumTV(cls):
         return cls.numTV
-    
+
     @classmethod
     def setNumTV(cls, numTV):
         cls.numTV = numTV
@@ -56,19 +56,19 @@ class TV:
 
     def getEstado(self):
         return self._estado
-    
+
     def canalUp(self):
-        if self._canal < 120 and self._estado == True : 
+        if self._canal != 120 and self._estado == True:
             self._canal += 1
 
     def canalDown(self):
-        if self._canal > 1 and self._estado == True :
+        if self._canal != 1 and self._estado == True:
             self._canal -= 1
 
     def volumenUp(self):
-        if self._volumen < 7 and self._estado == True :
+        if self._volumen != 7 and self._estado == True:
             self._volumen += 1
 
     def volumenDown(self):
-        if self._volumen > 0 and self._estado == True :
+        if self._volumen != 0 and self._estado == True:
             self._volumen -= 1
